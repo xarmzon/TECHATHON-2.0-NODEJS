b567e5190f1a0ff25a1a2616e7e1cb8b89a98056
rastaxarm <xarmzon@gmail.com>
2022-12-08 16:24:20 +0000

    update

--- week8_9/database/server.js
+++ week8_9/database/server.js
@@ -5,3 +5,18 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server Running on http://localhost:${PORT}`);
 });
+
+// //Database > MyStore
+// //collection Products
+//   //document
+//     // key:value
+//     {
+//      productName: "sdfsdfs",
+//      productPrice: 78,
+//      dateAdded: "",
+//     }
+// //collection Users
+//     //document
+//     {
+//       username: "",
+//     }
